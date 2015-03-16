645eca0b7c8a383f3b4c7303a9903833f18e48fd
Ivan K <ivan.kocienski@gmail.com>
2015-03-16 16:02:35 +0000

    obstacle colors

--- src/obstacle.cc
+++ src/obstacle.cc
@@ -22,6 +22,7 @@ void Obstacle::set_pos( float x, float y, float z, float a ) {
   m_height = z;
   m_angle  = a;
 
+  m_mesh.set_color( 2 + rand() % 7 );
   m_mesh.set_translation( m_position.to_vector3(m_height) );
   m_mesh.set_rotation( 0, a, 0 );
 
